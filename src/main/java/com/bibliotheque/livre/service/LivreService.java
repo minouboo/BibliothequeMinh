fea140703d6a17708d6d1013df9718f9df985e65
minouboo <minouboo369@yahoo.com>
2022-10-11 15:31:07 +0200

    ajouter/consulter/modifier/supprimer un livre

---
+++ src/main/java/com/bibliotheque/livre/service/LivreService.java
@@ -0,0 +1,24 @@
+package com.bibliotheque.livre.service;
+
+import java.util.List;
+
+import com.bibliotheque.livre.model.Livre;
+
+public interface LivreService {
+
+    //pour une liste de livre
+    List<Livre> getAllLivres();
+
+    //pour enregistrer un livre
+    Livre saveLivre(Livre livre);
+
+    //pour recuperer un livre avec son id
+    Livre getLivreById (Long id);
+
+    //pour modifier un livre
+    Livre updateLivre(Livre livre);
+
+    //pour supprimer un livre
+    void deleteLivreById (long id);
+
+}
---
+++ src/main/java/com/bibliotheque/livre/service/implement/LivreServiceImplement.java
@@ -0,0 +1,52 @@
+package com.bibliotheque.livre.service.implement;
+
+import com.bibliotheque.livre.data.LivreRepository;
+import com.bibliotheque.livre.model.Livre;
+import com.bibliotheque.livre.service.LivreService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.util.List;
+
+@Service
+public class LivreServiceImplement implements LivreService {
+
+    private LivreRepository livreRepository;
+
+    public LivreServiceImplement(LivreRepository livreRepository) {
+        super();
+        this.livreRepository = livreRepository;
+    }
+
+    //methode pour avoir une liste de livre
+    @Override
+    public List<Livre> getAllLivres() {
+        return livreRepository.findAll();
+    };
+
+    // methode pour sauvegarder un livre
+    @Override
+    public Livre saveLivre (Livre livre) {
+        return livreRepository.save(livre);
+    }
+
+    // methode pour recupere un livre avec Id
+    @Override
+    public Livre getLivreById(Long id) {
+
+        return livreRepository.findById(id).get();
+    }
+
+    // methode pour modifier un livre
+    @Override
+    public Livre updateLivre(Livre livre) {
+
+        return livreRepository.save(livre);
+    }
+
+    @Override
+    public void deleteLivreById(long id) {
+        livreRepository.deleteById(id);
+    }
+}
